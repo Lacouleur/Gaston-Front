d387e1657e318e28ff8e0ccfdfcbbc09918a47f9
Lacouleur <damien.voindrot@gmail.com>
2019-10-18 16:22:49 +0200

    Préparation de tousles composants Logs

---
+++ src/components/Logs/Inscription/index.js
@@ -0,0 +1,19 @@
+// == Import : npm
+import React from 'react';
+
+// == Import : local
+import './inscription.scss';
+
+
+// == Composant
+const Inscription = () => {
+
+    return (
+  <div className="inscription">
+
+  </div>
+);
+    }
+
+// == Export
+export default Inscription;
---
+++ src/components/Logs/Login/index.js
@@ -0,0 +1,19 @@
+// == Import : npm
+import React from 'react';
+
+// == Import : local
+import './login.scss';
+
+
+// == Composant
+const Login = () => {
+
+    return (
+  <div className="login">
+
+  </div>
+);
+    }
+
+// == Export
+export default Login;
