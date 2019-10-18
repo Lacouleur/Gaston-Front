348adf92c43e258b3357f398ae1b0f253086b4f8
Live session user <mint@etudiant.oclock.io>
2019-10-18 14:37:24 +0200

    Css & Map responsive

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,11 @@
 // == Import : npm
 import React from 'react';
+import { Container} from 'semantic-ui-react';
 
 // == Import : local
 import './app.scss';
 import NavBar from 'src/components/NavBar';
+import Content from 'src/components/Content';
 import Map from 'src/containers/Map';
 import Tresors from 'src/components/Tresors';
 import FooterNav from 'src/components/FooterNav';
@@ -11,10 +13,29 @@ import FooterNav from 'src/components/FooterNav';
 // == Composant
 const App = () => (
   <div id="app">
-    <NavBar />
-    <Map />
-    {/* <Tresors /> */}
-    <FooterNav />
+
+    <div className='navBar'>
+      
+    {/* <NavBar /> */}
+    </div>
+
+    <div className='mainContainer'>
+    
+      <div className="contentContainer">
+        {/*<Content/> */}
+       
+      </div>
+
+      <div className='mapContainer'>
+     
+      <Map />
+      </div>
+
+          {/* <Tresors /> */}
+
+
+          {/* <FooterNav /> */}
+        </div>
   </div>
 );
 
