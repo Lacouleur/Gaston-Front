2ccf7c543a485c88f10227e64f5715b374073406
AxelJr971 <jerentaxel@gmail.com>
2019-10-23 09:26:09 +0200

    nav color

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,16 +7,18 @@ import './app.scss';
 import Logs from 'src/components/Logs';
 import Map from 'src/containers/Map';
 import PostsList from 'src/components/PostsList';
+import NavBar from 'src/components/NavBar';
+
 // == Composant
 const App = () => {
-  //péparation d'un variable en dure pour affichage des composants dans "content" (navigation de gauche)
-  const view = "logs"
+  // péparation d'un variable en dure pour affichage des composants dans "content" (navigation de gauche)
+  const view = 'logs'
   return (
 
-  <div id="app">
-    <div className='navBar'>
-      Navigation
-    </div>
+  <div id='app'>
+
+<NavBar className='navBar'/>
+
     <div className="maincontainer">
       <div className="contentContainer">
         {view === 'PostsList' && <PostsList />}
