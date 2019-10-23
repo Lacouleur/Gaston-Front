8270062f6284560ed3911138ba038eafe5d9b367
Lacouleur <Lacouleur@github.com>
2019-10-23 09:22:47 +0200

    breudel de git

--- src/components/Logs/index.js
+++ src/components/Logs/index.js
@@ -1,6 +1,5 @@
 // == Import : npm
 import React from 'react';
-import PropTypes from 'prop-types';
 
 // == Import : local
 import './logs.scss';
@@ -9,52 +8,26 @@ import Inscription from './Inscription';
 import Login from './Login';
 import Welcome from './Welcome';
 
-/* Liste de taches 
-
-O - Faire un menu scrolable 
-O - Créer un "on submit" sur les forms
-  O- Avec le petit this.steState
-O - Ajouter des vérifications sur les inputs
-O - Mettre les click handlers
-
-
-
-*/
-
-
-
 // == Composant
-const Logs = ({changeHandler, changeView, view}) => {
+const Logs = () => {
 
-    console.log("VIEW(in Logs) :" , view);
+
+  const view = 'inscription'
 
     return (
   <div className="logs">
-    {view === 'welcome' && <Welcome 
-    changeView={changeView}
-    />}
-    {view === 'login' &&<Login 
-    changeHandler={changeHandler}
-    changeView={changeView}
-    />}
+    {view === 'welcome' && <Welcome />}
+    {view === 'login' &&<Login />}
     {view === 'inscription' &&<Inscription
-    changeHandler={changeHandler}
-    changeView={changeView}
-     />}
-    {view === 'forgot' &&<Forgot
-    changeHandler={changeHandler}
-    changeView={changeView}
-     />}
+    changeHandler = {this.changeHandler}
+
+    
+    />}
+    {view === 'forgot' &&<Forgot />}
   </div>
 );
     
  }
-
-Logs.propTypes = {
-  changeHandler: PropTypes.func.isRequired,
-  view : PropTypes.string.isRequired,
-  changeView: PropTypes.func.isRequired,
-};
 
 // == Export
 export default Logs;
