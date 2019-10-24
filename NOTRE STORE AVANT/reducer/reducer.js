d635c8f1ac57ea74cc2cd44310894944fafb7c6f
Lacouleur <Lacouleur@github.com>
2019-10-24 16:19:05 +0200

    Step1 : CombineReducer && Store

---
+++ NOTRE STORE AVANT/reducer/reducer.js
@@ -0,0 +1,23 @@
+// == Import : npm
+import { createStore, compose, applyMiddleware } from 'redux';
+// ici je vais trouver le reducer parent dans lequel je vais combiner mes reducers
+
+// redux fourni une fonction combineReducers qui combine plusieurs reducer en 1
+import { combineReducers } from 'redux';
+
+// on importe chacun des sous-reducers
+import loginReducer from 'src/store/reducer/LogsReducer/loginReducer';
+import inscriptionReducer from 'src/store/reducer/LogsReducer/incriptionReducer'
+import mapReducer from 'src/store/reducer/MapReducer/mapReducer'
+import postReducer from 'src/store/reducer/PostReducer/postReducer'
+// https://redux.js.org/api/combinereducers
+const reducer = combineReducers({
+  // user: user
+login : loginReducer,
+inscription : inscriptionReducer,
+forgot : forgotReducer,
+map : mapReducer, 
+post : postReducer
+});
+
+export default reducer;
--- src/store/reducer/reducer.js
+++ src/store/reducer/reducer.js
@@ -1,23 +1,26 @@
-// == Import : npm
-import { createStore, compose, applyMiddleware } from 'redux';
 // ici je vais trouver le reducer parent dans lequel je vais combiner mes reducers
 
 // redux fourni une fonction combineReducers qui combine plusieurs reducer en 1
 import { combineReducers } from 'redux';
 
 // on importe chacun des sous-reducers
-import loginReducer from 'src/store/reducer/LogsReducer/loginReducer';
-import inscriptionReducer from 'src/store/reducer/LogsReducer/incriptionReducer'
-import mapReducer from 'src/store/reducer/MapReducer/mapReducer'
-import postReducer from 'src/store/reducer/PostReducer/postReducer'
+import user from './user';
+import repos from './repos';
+import form from './form';
+import InputSearch  from './InputSearch';
+import results from './results'
+
+// combineReducers nous retourne le reducer parent généré
+// on doit fournir en paramètre un objet
+// avec en clé un nom donné à chaque reducer et en valeur, chaque fonction reducer
 // https://redux.js.org/api/combinereducers
 const reducer = combineReducers({
   // user: user
-loginReducer,
-inscriptionReducer,
-forgotReducer,
-mapReducer, 
-postReducer
+  user,
+  repos,
+  form,
+  InputSearch,
+  results,
 });
 
 export default reducer;
