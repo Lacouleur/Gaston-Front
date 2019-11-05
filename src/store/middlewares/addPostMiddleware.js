eb5debc99c4e0b47700c6215cbb04df9ef8a47b2
coolskin2b <coolskin2b@gmail.com>
2019-11-05 09:58:05 +0100

    Need To wait Back

--- src/store/middlewares/addPostMiddleware.js
+++ src/store/middlewares/addPostMiddleware.js
@@ -4,24 +4,29 @@ import axios from 'axios';
 export const ADD_POST = 'add_post';
 export const ADDPOST_ERROR = 'addpost_error';
 
-import { UrlDev, UrlProd } from "src/store/middlewares/connect_data.js";
-
+import { UrlDev, UrlProd } from 'src/store/middlewares/connect_data.js';
 
 export function addPostAction(values) {
   console.log('addPost is working');
-  console.log(localStorage.user);
+  console.log('======>', values);
   const JWTToken = localStorage.user;
+  const headers = {
+    headers: { Authorization: `Bearer ${JWTToken}` },
+  };
+  console.log(values);
   return async (dispatch) => {
     try {
-      const res = await axios.post(`${UrlDev}/api/post-new`,
-      { headers: { Authorization: `Bearer ${JWTToken}` }, 
-      ...values });
-     dispatch({ type: ADD_POST });
+      const res = await axios.post(
+        `${UrlDev}/api/post-new`,
+        { ...values },
+        headers,
+      );
+      dispatch({ type: ADD_POST });
 
       // localStorage.setItem('user', res.data.token);
       history.push('/postlist');
     } catch (error) {
-        console.log(error);
+      console.log(error);
       dispatch({
         type: ADDPOST_ERROR,
         payloadAddPost: 'Erreur Formulaire',
