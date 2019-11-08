37674474bfbfb28912b2e84ec3c07179bd47b9c3
Lacouleur <damien.voindrot@gmail.com>
2019-11-08 12:17:32 +0100

    Test Post + test prod

--- src/store/middlewares/addPostMiddleware.js
+++ src/store/middlewares/addPostMiddleware.js
@@ -8,6 +8,7 @@ import { UrlProd } from 'src/store/middlewares/connect_data.js';
 import {
   RECEIVE_POSTS,
   FETCH_POSTS,
+  addPostFromUser,
 } from 'src/store/reducer/PostsListReducer/postsListReducer';
 export function addPostAction(values) {
   // var bodyFormData = new FormData();
@@ -22,13 +23,18 @@ export function addPostAction(values) {
   };
   return async (dispatch) => {
     try {
+      console.log('Posting post...');
       const res = await axios.post(`${UrlProd}/api/post-new`, values, headers);
+      console.log("res.data l26 addPostMiddlzWare =>", res.data);
+      //res.data
       dispatch({ type: ADD_POST });
       // localStorage.setItem('user', res.data.token);
       //     console.log('je cherche a reprendre la liste des postes apres une publication')
       dispatch({
         type: FETCH_POSTS,
       });
+      const addPostFromUserAction = addPostFromUser(res.data);
+      store.dispatch(addPostFromUserAction);
     } catch (error) {
       console.log(error);
       dispatch({
--- src/store/reducer/PostsListReducer/postsListReducer.js
+++ src/store/reducer/PostsListReducer/postsListReducer.js
@@ -20,15 +20,16 @@ const STOP_LOAD = 'STOP_LOAD';
 export const FETCH_USERS = 'FETCH_USERS';
 export const STORE_POSTS_DETAILS = 'STORE_POSTS_DETAILS';
 export const FETCH_POSTS_DETAILS = 'FETCH_POSTS_DETAILS';
+const ADD_POST_FROM_USER = 'ADD_POST_FROM_USER'
 
 export const FETCH_CATEGORIES = 'FETCH_CATEGORIES';
 // - Reducer
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
-    // case FETCH_POSTS:
-    //   return {
-    //     ...state,
-    //   };
+    case FETCH_POSTS:
+      return {
+        ...state,
+      };
     case RECEIVE_POSTS:
       return {
         ...state,
@@ -37,12 +38,19 @@ const reducer = (state = initialState, action = {}) => {
       };
     case STORE_POSTS_DETAILS:
       console.log('JE COMPLETE LES DETAILS DES POSTS', action);
-
       return {
         ...state,
         postsListsDetails: [...state.postsListsDetails, action.postDetails],
         loading: true,
       };
+    case ADD_POST_FROM_USER:
+      console.log('je suis une action :  ADD_POST_FROM_USER, ', action.addPostfromUser)
+      return {
+        ...state,
+        postsListsDetails: [...state.postsListsDetails, action.addPostfromUser],
+        loading: true,
+      };
+
     case STOP_LOAD:
       return {
         ...state,
@@ -65,7 +73,10 @@ export const receivePosts = (posts) => ({
   type: RECEIVE_POSTS,
   posts,
 });
-
+export const addPostFromUser = (addPostfromUser) => ({
+  type: ADD_POST_FROM_USER,
+  addPostfromUser,
+});
 export const fetchPostsDetails = () => ({
   type: FETCH_POSTS_DETAILS,
 });
